387218de7f2c3aa72730199550cfe290504b1ab1
Sashe <sash239@gmail.com>
2016-06-04 19:35:27 +0200

    minor fixes

--- .emacs.d.old/elpa/darkokai-theme-20160512.816/darkokai-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "darkokai-theme" "20160512.816" "A darker variant on Monokai." 'nil :url "http://github.com/sjrmanning/darkokai")
--- .emacs.d.old/elpa/dash-20160510.1127/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (22349 62843 21246 105000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
